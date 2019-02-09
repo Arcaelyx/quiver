6e4426df3f86a2bf35b0f89948ad8c723f67a56f
Arcaelyx <arcaelyx@protonmail.com>
2019-02-09 00:05:15 -0500

    Provide route method for saving email subscriptions
    In order to process POST requests with JSON payloads containing email
    data, a method has been created which integrates with Scotty & SQLite3.
    
    A UUID v4 is generated to be used as the email's ID to be referenced
    against along with the email which is the inserted into the database.

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,20 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric, OverloadedStrings, ScopedTypeVariables #-}
 
 module Main where
 
+import Control.Monad.IO.Class (liftIO)
 import Data.Aeson
+import Data.Text.Lazy
+import Data.UUID (toString)
 import Database.SQLite.Simple
 import GHC.Generics
+import Network.HTTP.Types
 import Network.Wai.Middleware.RequestLogger
 import System.Environment
 import Web.Scotty
+import Web.Scotty.Internal.Types (ActionT)
+
+import qualified Data.UUID.V4 as UUIDV4
 
 data Email = Email String deriving (Generic, Show)
 
@@ -20,6 +27,13 @@ insertQuery = "INSERT INTO subscriber (id, email) VALUES (?, ?)"
 
 fromEmail :: Email -> String
 fromEmail (Email email) = email
+
+createSubscriber :: Connection -> ActionT Text IO ()
+createSubscriber connection = do
+  (email :: Email) <- jsonData
+  id <- liftIO $ UUIDV4.nextRandom
+  liftIO $ execute connection insertQuery (toString id, fromEmail email)
+  status status201
 
 main :: IO ()
 main = do
